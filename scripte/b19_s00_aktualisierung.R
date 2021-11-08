59c72b8064a9b04a907f8a29a96d6b6d6fb6d711
holgerman <holgerkirsten@web.de>
2021-11-08 13:32:34 +0100

    update

--- scripte/b19_s00_aktualisierung.R
+++ scripte/b19_s00_aktualisierung.R
@@ -3,7 +3,7 @@ rm(list = ls())
 set.seed(192)
 
 knitr::stitch_rhtml(here::here("scripte/b19_s06_3_ageStratIncidencePlot_211014.R"))
-knitr::stitch_rhtml(here::here("scripte/b19_s10_3_vaccNonVaccPlotData.R"))
+knitr::stitch_rhtml(here::here("scripte/b19_s10_4_vaccNonVaccPlotData.R"))
 knitr::stitch_rhtml(here::here("scripte/b19_s11_2_farrington_rki.R"))
 knitr::stitch_rhtml(here::here("scripte/b19_s14_2_yearComparisonGerSax_211028.R"))
 
