5023e750d7de064c6bf926ce8d1657c90893358f
holgerman <holgerkirsten@web.de>
2021-11-08 11:57:26 +0100

    update figures

---
+++ scripte/b19_s00_aktualisierung.R
@@ -0,0 +1,10 @@
+# # Initialize ----
+rm(list = ls())
+set.seed(192)
+
+knitr::stitch_rhtml(here::here("scripte/b19_s06_3_ageStratIncidencePlot_211014.R"))
+knitr::stitch_rhtml(here::here("scripte/b19_s11_2_farrington_rki.R"))
+knitr::stitch_rhtml(here::here("scripte/b19_s14_2_yearComparisonGerSax_211028.R"))
+
+# # finalize -----
+sessionInfo() # verwendete packages und software fuer dokumentationszwecke
